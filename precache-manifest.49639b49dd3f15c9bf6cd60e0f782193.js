b55ad7a3cd3f4d21756e725745f0f3c9d7cfa63d
matheus-gio <matheus.giovanni.lu@gmail.com>
2020-04-20 20:27:59 -0300

    Add files via upload

---
+++ precache-manifest.49639b49dd3f15c9bf6cd60e0f782193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a010d544824df628072ac69bbede5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dbd98a3cfbb8ae0512f",
+    "url": "/static/js/2.dd6186e1.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/static/js/2.dd6186e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039e7bb230c46ca1eef9",
+    "url": "/static/js/main.4a116f7c.chunk.js"
+  },
+  {
+    "revision": "0c0d42bc3317a63ff725",
+    "url": "/static/js/runtime-main.be4647b1.js"
+  },
+  {
+    "revision": "9fff9ac9993a40022cbcfc12616644d5",
+    "url": "/static/media/instrutor.9fff9ac9.png"
+  },
+  {
+    "revision": "089f3c62256c324c8484cc6486c8e836",
+    "url": "/static/media/logo-academy.089f3c62.png"
+  }
+]);
